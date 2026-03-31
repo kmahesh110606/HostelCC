ed3ea82c5b0adc305acd31edc454cafba5f8251c
K. Mahesh Chandran <kmahesh110606@outlook.com>
2026-03-31 14:31:17 +0530

    Changed app loc

--- app/android/app/src/main/kotlin/com/example/hostel_cc_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hostel_cc_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- app/lib/services/notification_attachment_cache.dart
+++
@@ -1,2 +0,0 @@
-export "notification_attachment_cache_stub.dart"
-    if (dart.library.io) "notification_attachment_cache_io.dart";
