ad7b6a8eae0f488267c8e8ed171cbcd5e840972f
K. Mahesh Chandran <kmahesh110606@outlook.com>
2026-04-03 21:38:09 +0530

    OTP

--- hostel_app/settings.py
+++ hostel_app/settings.py
@@ -177,9 +177,9 @@ EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.Email
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_USER = "1442space@gmail.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = "1442space@gmail.com"
 DEFAULT_OTP_CODE = os.getenv("DEFAULT_OTP_CODE", "").strip()
 
 if DEFAULT_OTP_CODE and not DEBUG:
